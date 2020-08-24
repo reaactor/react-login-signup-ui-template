703068504385b4ed7e2a68e732072e1107097c10
ayushpandey13 <>
2020-08-24 17:33:57 +0530

    Updates

---
+++ precache-manifest.4a24bccb1e5dfc256a33c0cd3a0b7742.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e46c0cb44d3ba637f92dcedfa2aeb4",
+    "url": "/react-login-signup-ui-template/index.html"
+  },
+  {
+    "revision": "48b62d1ba7b4fa0b20d6",
+    "url": "/react-login-signup-ui-template/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "54c284d16780a183f41c",
+    "url": "/react-login-signup-ui-template/static/css/main.cfc4900b.chunk.css"
+  },
+  {
+    "revision": "48b62d1ba7b4fa0b20d6",
+    "url": "/react-login-signup-ui-template/static/js/2.83fb8aa1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-login-signup-ui-template/static/js/2.83fb8aa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54c284d16780a183f41c",
+    "url": "/react-login-signup-ui-template/static/js/main.3a896825.chunk.js"
+  },
+  {
+    "revision": "7f904732d9829ea1546a",
+    "url": "/react-login-signup-ui-template/static/js/runtime-main.fa89d72d.js"
+  }
+]);
